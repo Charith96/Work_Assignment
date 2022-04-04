67a289771ca7f7913ed7de3404c4a78bcfe46ba7
Charith <wcharithbuddhipriya@gmail.com>
2022-04-04 16:36:36 +0530

    remove id field

--- client/src/Components/AddMember.jsx
+++ client/src/Components/AddMember.jsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
 import axios from "axios";
+import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -18,15 +19,13 @@ import {
 
 const AddMember = () => {
     const [values, setValues] = useState({
-        id: "",
         name: "",
         country: "",
         address: ""
     });
 
+    //validate form field
     const validationSchema = Yup.object().shape({
-        id: Yup.string()
-            .required('Id is required number*'),
         name: Yup.string()
             .required('Name is required*')
             .min(5, 'Name must be at least 5 characters*'),
@@ -53,13 +52,13 @@ const AddMember = () => {
 
         try {            
             const response = await axios.get(`http://localhost:8010/v1/countries/`);
-            if (!response.data.error) setCounties(response.data.countries);
+            if (response.status === 200) setCounties(response.data.countries);
 
         } catch (error) {            
             toast(error.response.data.message, { type: toast.TYPE.ERROR });
         }
     }
-
+    //handle user input
     const handleChange = (e) => {
         setValues({
             ...values,
@@ -67,7 +66,7 @@ const AddMember = () => {
         });
     };
 
-
+    //submit data database if no errors
     const onSubmit = async (data) => {
 
         try {
@@ -87,7 +86,6 @@ const AddMember = () => {
 
         } catch (error) {
             toast(error.response.data.message, { type: toast.TYPE.ERROR });
-            //toast.error("You have an Error in Adding members 🛑");
         }
     }
 
@@ -96,95 +94,85 @@ const AddMember = () => {
         <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}>
 
             <Container className='body-wrapper'>
-                <div className='title'>
-                    <h1><span style={{ border: '2px solid gray' }} />&nbsp;Add Members</h1>
-                </div>
 
-                <div className='content'>
-                    <Row>
+                <div className='form-content'>
+                    <Row>                        
                         <Col />
                         <Col id='form'>                            
-                            <Form onSubmit={handleSubmit(onSubmit)}>
-
-                                <div className="input-group mb-3">
-                                    <span className="input-group-text" id="basic-addon1">
-                                        <i className="fas fa-id-card" />
-                                    </span>
-                                    <input
-                                        name="id"
-                                        type="number"
-                                        placeholder="ID Number"
-                                        // value={values.id}
-                                        onChange={handleChange}
-                                        {...register('id')}
-                                        className={`form-control ${errors.id ? 'is-invalid' : ''}`}
-                                    />
-                                    <div className="invalid-feedback">{errors.id?.message}</div>
+                            <Row id='form-title'>
+                                <div id='text'>
+                                    <h2>Register Member</h2>
                                 </div>
+                            </Row>
+                            <Row id='form-field'>
+                                <Form onSubmit={handleSubmit(onSubmit)}>
+                                    <div className="input-group mb-3">
+                                        <span className="input-group-text" id="basic-addon1">
+                                            <i className="fas fa-user" />
+                                        </span>
+                                        <input
+                                            name="name"
+                                            type="text"
+                                            placeholder="Full Name"
+                                            id='input'
+                                            onChange={handleChange}
+                                            // value={values.name}
+                                            {...register('name')}
+                                            className={`form-control ${errors.name ? 'is-invalid' : ''}`} />
+                                        <div className="invalid-feedback">{errors.name?.message}</div>
+                                    </div>
 
-                                <div className="input-group mb-3">
-                                    <span className="input-group-text" id="basic-addon1">
-                                        <i className="fas fa-user" />
-                                    </span>
-                                    <input
-                                        name="name"
-                                        type="text"
-                                        placeholder="Full Name"
-                                        onChange={handleChange}
-                                        // value={values.name}
-                                        {...register('name')}
-                                        className={`form-control ${errors.name ? 'is-invalid' : ''}`} />
-                                    <div className="invalid-feedback">{errors.name?.message}</div>
-                                </div>
+                                    <div class="input-group mb-3">
+                                        <label className="input-group-text" for="inputGroupSelect01">
+                                            <i className="fas fa-flag" />
+                                        </label>
+                                        <select
+                                            name="country"
+                                            // value={values.country}
+                                            id='input'
+                                            onChange={handleChange}
+                                            {...register('country')}
+                                            className={`form-select ${errors.country ? 'is-invalid' : ''}`}
+                                        >
+                                            <option value="">Select Country</option>
 
-                                <div class="input-group mb-3">
-                                    <label className="input-group-text" for="inputGroupSelect01">
-                                        <i className="fas fa-flag" />
-                                    </label>
-                                    <select
-                                        name="country"
-                                        // value={values.country}
-                                        onChange={handleChange}
-                                        {...register('country')}
-                                        className={`form-select ${errors.country ? 'is-invalid' : ''}`}
-                                    >
-                                        <option value="">Select Country</option>
+                                            {/* retrieve countries in country collection */}
+                                            {countries.map((country, key) => {
+                                                return (
+                                                    <option value={country.name} >{country.name}</option>
+                                                );
+                                            })}
+                                        </select>
+                                        <div className="invalid-feedback">{errors.country?.message}</div>
+                                    </div>
 
-                                        {/* retrieve countries in country collection */}
-                                        {countries.map((country, key) => {
-                                            return (
-                                                <option value={country.name} >{country.name}</option>
-                                            );
-                                        })}
-                                    </select>
-                                    <div className="invalid-feedback">{errors.country?.message}</div>
-                                </div>
+                                    <div className="input-group mb-3">
+                                        <span className="input-group-text" id="basic-addon1">
+                                            <i className="fas fa-map-marker-alt" />
+                                        </span>
+                                        <textarea
+                                            name="address"
+                                            type="text"
+                                            rows={1}
+                                            placeholder="Address"
+                                            id='input'
+                                            onChange={handleChange}
+                                            // value={values.address}
+                                            {...register('address')}
+                                            className={`form-control ${errors.address ? 'is-invalid' : ''}`}
+                                        />
+                                        <div className="invalid-feedback">{errors.address?.message}</div>
+                                    </div>
 
-                                <div className="input-group mb-3">
-                                    <span className="input-group-text" id="basic-addon1">
-                                        <i className="fas fa-map-marker-alt" />
-                                    </span>
-                                    <textarea
-                                        name="address"
-                                        type="text"
-                                        rows={1}
-                                        placeholder="Address"
-                                        onChange={handleChange}
-                                        // value={values.address}
-                                        {...register('address')}
-                                        className={`form-control ${errors.address ? 'is-invalid' : ''}`}
-                                    />
-                                    <div className="invalid-feedback">{errors.address?.message}</div>
-                                </div>
-
-                                <Row style={{ marginTop: '40px' }}>
-                                    <Col>
-                                        <Button variant="primary" type="submit" style={{ width: '100%' }}>
-                                            Create
-                                        </Button>
-                                    </Col>
-                                </Row>
-                            </Form>
+                                    <Row style={{ marginTop: '40px' }}>
+                                        <Col>
+                                            <Button variant="primary" type="submit" style={{ width: '100%', padding:'10px' }}>
+                                                Create
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </Row>
                         </Col>
                         <Col />
                     </Row>
--- client/src/Components/EditMember.jsx
+++ client/src/Components/EditMember.jsx
@@ -17,16 +17,9 @@ import {
 } from "react-bootstrap";
 
 const EditMember = () => {
-    const [values, setValues] = useState({
-        id: "",
-        name: "",
-        country: "",
-        address: ""
-    });
 
+    //validate form field
     const validationSchema = Yup.object().shape({
-        id: Yup.string()
-            .required('Id is required number*'),
         name: Yup.string()
             .required('Name is required*')
             .min(5, 'Name must be at least 5 characters*'),
@@ -46,7 +39,6 @@ const EditMember = () => {
 
 
     const {
-        id,
         name,
         country,
         address
@@ -64,20 +56,20 @@ const EditMember = () => {
 
         try {            
             const response = await axios.get(`http://localhost:8010/v1/countries/`);
-            if (!response.data.error) setCounties(response.data.countries);
+            if (response.status === 200) setCounties(response.data.countries);
 
         } catch (error) {            
             toast(error.response.data.message, { type: toast.TYPE.ERROR });
         }
     }
 
-    //fetching members
+    //fetching member by id
     const fetchMembers = async () => {
         try {
 
             const response = await axios.get(`http://localhost:8010/v1/members/${params.id}`);
             
-            if (!response.data.error) {
+            if (response.status === 200) {
                 setMember(response.data.member);
                 console.log(response.data.member);
             }
@@ -113,107 +105,95 @@ const EditMember = () => {
 
         } catch (error) {
             toast(error.response.data.message, { type: toast.TYPE.ERROR });
-            //toast.error("You have an Error in Adding members 🛑");
         }
     }
-
-
 
 
     return (
         <ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}>
 
             <Container className='body-wrapper'>
-                <div className='title'>
-                    <h1><span style={{ border: '2px solid gray' }} />&nbsp;Edit Members</h1>
-                </div>
 
                 <div className='content'>
                     <Row>
                         <Col />
                         <Col id='form'>
-                            <Form onSubmit={handleSubmit(onSubmit)}>
-
-                                <div className="input-group mb-3">
-                                    <span className="input-group-text" id="basic-addon1">
-                                        <i className="fas fa-id-card" />
-                                    </span>
-                                    <input
-                                        name="id"
-                                        type="number"
-                                        placeholder="ID Number"
-                                        value={id}
-                                        {...register('id')}
-                                        className={`form-control ${errors.id ? 'is-invalid' : ''}`}
-                                        onChange={handleChange}
-                                    />
-                                    <div className="invalid-feedback">{errors.id?.message}</div>
+                            <Row id='form-title'>
+                                <div id='text'>
+                                    <h2>Edit Member</h2>
                                 </div>
+                            </Row>      
+                            <Row id='form-field'>
+                                <Form onSubmit={handleSubmit(onSubmit)}>
 
-                                <div className="input-group mb-3">
-                                    <span className="input-group-text" id="basic-addon1">
-                                        <i className="fas fa-user" />
-                                    </span>
-                                    <input
-                                        name="name"
-                                        type="text"
-                                        placeholder="Full Name"
-                                        value={name}
-                                        {...register('name')}
-                                        className={`form-control ${errors.name ? 'is-invalid' : ''}`} 
-                                        onChange={handleChange}
+                                    <div className="input-group mb-3">
+                                        <span className="input-group-text" id="basic-addon1">
+                                            <i className="fas fa-user" />
+                                        </span>
+                                        <input
+                                            name="name"
+                                            type="text"
+                                            id='input'
+                                            placeholder="Full Name"
+                                            value={name}
+                                            {...register('name')}
+                                            className={`form-control ${errors.name ? 'is-invalid' : ''}`} 
+                                            onChange={handleChange}                                            
+                                            />
+                                        <div className="invalid-feedback">{errors.name?.message}</div>
+                                    </div>
+
+                                    <div class="input-group mb-3">
+                                        <label className="input-group-text" for="inputGroupSelect01">
+                                            <i className="fas fa-flag" />
+                                        </label>
+                                        <select
+                                            name="country"
+                                            value={country}
+                                            id='input'
+                                            {...register('country')}
+                                            className={`form-select ${errors.country ? 'is-invalid' : ''}`}
+                                            onChange={handleChange}
+                                        >
+                                            <option value="">Select Country</option>
+
+                                            {/* retrieve countries in country collection */}
+                                            {countries.map((country, key) => {
+                                                return (
+                                                    <option value={country.name} >{country.name}</option>
+                                                );
+                                            })}
+                                        </select>
+                                        <div className="invalid-feedback">{errors.country?.message}</div>
+                                    </div>
+
+                                    <div className="input-group mb-3">
+                                        <span className="input-group-text" id="basic-addon1">
+                                            <i className="fas fa-map-marker-alt" />
+                                        </span>
+                                        <textarea
+                                            name="address"
+                                            type="text"
+                                            id='input'
+                                            rows={1}
+                                            placeholder="Address"
+                                            value={address}
+                                            {...register('address')}
+                                            className={`form-control ${errors.address ? 'is-invalid' : ''}`}
+                                            onChange={handleChange}
                                         />
-                                    <div className="invalid-feedback">{errors.name?.message}</div>
-                                </div>
+                                        <div className="invalid-feedback">{errors.address?.message}</div>
+                                    </div>
 
-                                <div class="input-group mb-3">
-                                    <label className="input-group-text" for="inputGroupSelect01">
-                                        <i className="fas fa-flag" />
-                                    </label>
-                                    <select
-                                        name="country"
-                                        value={country}
-                                        {...register('country')}
-                                        className={`form-select ${errors.country ? 'is-invalid' : ''}`}
-                                        onChange={handleChange}
-                                    >
-                                        <option value="">Select Country</option>
-
-                                        {/* retrieve countries in country collection */}
-                                        {countries.map((country, key) => {
-                                            return (
-                                                <option value={country.name} >{country.name}</option>
-                                            );
-                                        })}
-                                    </select>
-                                    <div className="invalid-feedback">{errors.country?.message}</div>
-                                </div>
-
-                                <div className="input-group mb-3">
-                                    <span className="input-group-text" id="basic-addon1">
-                                        <i className="fas fa-map-marker-alt" />
-                                    </span>
-                                    <textarea
-                                        name="address"
-                                        type="text"
-                                        rows={1}
-                                        placeholder="Address"
-                                        value={address}
-                                        {...register('address')}
-                                        className={`form-control ${errors.address ? 'is-invalid' : ''}`}
-                                        onChange={handleChange}
-                                    />
-                                    <div className="invalid-feedback">{errors.address?.message}</div>
-                                </div>
-
-                                <Row style={{ marginTop: '40px' }}>
-                                    <Col>
-                                        <Button variant="primary" type="submit" style={{ width: '100%' }}>
-                                            Update
-                                        </Button>
-                                    </Col>
-                                </Row>
-                            </Form>
+                                    <Row style={{ marginTop: '40px' }}>
+                                        <Col>
+                                            <Button variant="primary" type="submit" style={{ width: '100%' }}>
+                                                Update
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </Row>                      
                         </Col>
                         <Col />
                     </Row>
