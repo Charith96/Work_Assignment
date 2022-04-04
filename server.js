7314c8b22ae0d6734475942599c61439e12f9514
Charith <wcharithbuddhipriya@gmail.com>
2022-04-04 13:27:03 +0530

    connection error log fixed

--- server.js
+++ server.js
@@ -30,7 +30,6 @@ app.listen(PORT, async () => {
         await mongoose.connect(MONGO_URI, {
             useNewUrlParser: true
         });
-
         console.log("MonoDB successfully connected 🔥🟢");        
         console.log(`Server is running on port ${PORT}`);
 
